6f2ed3a63a01e3e2d3d00e1804a726c3014b2da2
Melek Akkoyunlu <melekakkoyunlu3@gmail.com>
2024-02-06 15:33:30 +0300

    Siralama.java added

--- src/Siralama/Siralama.java
+++ src/Siralama/Siralama.java
@@ -2,21 +2,41 @@ package Siralama;
 import java.util.Scanner;
 public class Siralama {
     public static void main(String[] args) {
-        int num1 , num2, num3 , temp;
+        int a, b, c;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter 3 integer:");
-        num1=input.nextInt();num2=input.nextInt();num3=input.nextInt();
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
 
-        temp= num1;
-        if(temp<=num2 && num2>=num3){
-            temp=num2;
-        } else if (temp<=num3 && num2<=num3) {
-            temp =num3;
-        }else{
-            temp=num1;
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.println(c + " " + b + " " + a);
+
+            } else {
+                System.out.println(b + " " + c + " " + a);
+
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a > c) {
+                System.out.println(c + " " + a + " " + b);
+
+            } else {
+                System.out.println(a + " " + c + " " + b);
+
+            }
+
+        } else if ((c > a) && (c > b)) {
+
+            if (a > b) {
+                System.out.println(b + " " + a + " " + c);
+
+            } else {
+                System.out.println(a + " " + b + " " + c);
+
+            }
+
         }
-        System.out.println("Biggest:" + temp);
-
     }
 }
